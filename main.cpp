e73612d8aa974b58b043816b0e2d08458778f6fe
Tatiana Malinaricova <tatiana.malinaricova@gmail.com>
2018-09-21 14:43:16 +0200

    vypis, cviko1

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
 	std::cout << "Hello World!" << std::endl;
+	std::string x;
+	getline(std::cin,x);
+	std::cout << x << std::endl;
+
 
 	return 0;
 }
